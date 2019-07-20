66d6e6bf263146506d3af5719b10671139ab1e52
MD. Shams Wadud <abbir.ku@gmail.com>
2019-07-21 00:33:06 +0600

    Add DbContext

---
+++ AgentManagement.Core/DBContext/EFDBContext.cs
@@ -0,0 +1,28 @@
+﻿using AgentManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentManagement.Core.DBContext
+{
+    public class EFDBContext : DbContext
+    {
+        public EFDBContext()
+            : base("name=DatabaseContext")
+        {
+        }
+
+        public DbSet<BusinessEntities> BusinessEntities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var conventions = new List<PluralizingTableNameConvention>().ToArray();
+            modelBuilder.Conventions.Remove(conventions);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
